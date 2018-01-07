d5e12edcbb4be43829e3c71b214e91c4cde69142
erik oosting <erik99.oosting@gmail.com>
2018-01-07 11:32:38 +0100

    Update makeSites.sh

--- makeSites.sh
+++ makeSites.sh
@@ -2,7 +2,7 @@ node getDB.js > data.json
 node createSites.js
 cd docs/
 currentDate=`date`
-git add .
-git pull origin master
-git commit -m "Directory updated on: $currentDate"
-git push origin master
+#git add .
+#git pull origin master
+#git commit -m "Directory updated on: $currentDate"
+#git push origin master
