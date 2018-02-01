c03d63a0fd2c0879d67a81b57e70b6bcb1dd49a6
erik99.oosting <erik99.oosting@gmail.com>
2018-02-01 05:47:56 -0500

    Directory updated on: Thu Feb  1 05:47:55 EST 2018

--- makeSites.sh
+++ makeSites.sh
@@ -2,7 +2,7 @@ node getDB.js > data.json
 node createSites.js
 cd docs/
 currentDate=`date`
-git add .
-git pull origin CA
+git add -A ..
+git pull origin master
 git commit -m "Directory updated on: $currentDate"
-git push origin CA
+git push origin master
