5689d8f513024464b409394218e54e838c49669b
erik oosting <erik99.oosting@gmail.com>
2018-02-16 15:22:04 +0100

    Delete makeSites.sh

--- startpagina-CA/makeSites.sh
+++
@@ -1,8 +0,0 @@
-node getDB.js > data.json
-node createSites.js
-cd docs/
-currentDate=`date`
-git add .
-git pull origin master
-git commit -m "Directory updated on: $currentDate"
-git push origin master
